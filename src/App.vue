94c64e08b47529d585c9fd11a6265690eb7ab30c
Юлия Зайцева <y.zaytzeva@fonmix.ru>
2024-08-13 14:17:24 +0300

    add layout

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,25 @@
 </script>
 
 <template>
+  <header>
+     
+  </header>
+  <aside>
+    <h3>Меню</h3>
+  </aside>
   <router-view></router-view>
+  <footer></footer>
 </template>
 
 <style>
-
+ aside {
+  background-color: grey;
+  width: 300px;
+  height: 100vh;
+ }
+ footer, header {
+  width: 100%;
+  height: 80px;
+  background: black;
+ }
 </style>
